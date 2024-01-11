a8d61fca5cb83317c850bd147fac2d66cc9ef392
timileyin42 <akanmuibro@gmail.com>
2024-01-10 23:39:09 -0800

    pycode fix

---
+++ models/state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+states class that inherits from BaseModel
+"""
+
+from models.base_models import BaseModel
+
+
+class state(baseModel):
+
+    """ Class that defines attributes for a state """
+
+    name = ""
