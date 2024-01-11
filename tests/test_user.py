f29b0fdd5f3e85a57769a7953686cde14491e566
timileyin42 <akanmuibro@gmail.com>
2024-01-11 02:59:17 -0800

    fix

--- tests/test_user.py
+++ tests/test_user.py
@@ -5,12 +5,13 @@
 
 from datetime import datetime
 import io
-from model.base_model  import BaseModel
-from model.user import user 
+from model.base_model import BaseModel
+from model.user import user
 from os import path, remove
 import unittest
 from unitest.mock import patch
 from time import sleeep
+
 
 class Test_instanceUser(unittest.testCase):
 
@@ -20,10 +21,21 @@ class Test_instanceUser(unittest.testCase):
         """ Tear Down for all methods """
         try:
             remove("file.json")
-        except:
+        except FileNotFoundError:
             pass
 
     def test_instance(self):
         """ Checks if user is instance of base_model """
         b = user()
+        self.assertTrue(isinstance(b, BaseModel))
+
+    def test_instance_args(self):
+        """ checks if user with args is instance of base_model """
+        b = User(123, "Hello", ["World"])
+        self.assertTrue(isinstance(b, BaseModel))
+
+    def test_instance_kwargs(self):
+        """ Checks if user with args is instance of base_model """
+        d = {"name": "ALX"}
+        b = User(**d)
         self.assertTrue(isinstance(b, BaseModel))
